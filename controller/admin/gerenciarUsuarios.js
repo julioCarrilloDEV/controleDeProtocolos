61ff05e971b97c504ad440948d7f8d7eb61c4410
julioCarrilloDEV <jc.carrillodev@gmail.com>
2024-11-19 15:11:39 -0300

    Criado a funcionalidade de liberar protocolos por usuário pelo admin.

--- controller/admin/gerenciarUsuarios.js
+++ controller/admin/gerenciarUsuarios.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
     chamarView();
     chamarAssociacoes();
     salvarAssociacoes();
+    chamarProtocolos();
+    salvarProtocolos();
 
     function carregarUsuarios() {
         $.ajax({
@@ -17,16 +19,19 @@ $(document).ready(function(){
                         <tr>
                             <td>${usuario.nome}</td> 
                             <td class="text-center">
-                                <button class="btn btn-sm btn-dark assocCat-btn" data-id="${usuario.id}" data-nome="${usuario.nome}">
-                                    <i class="bi bi-calendar2-plus"></i>
+                                <button title="Associar categorias" class="btn btn-sm btn-dark assocCat-btn" data-id="${usuario.id}" data-nome="${usuario.nome}">
+                                    <i class="bi bi-grid-1x2"></i>
                                 </button>
-                                <button class="btn btn-sm btn-primary view-btn" data-id="${usuario.id}" data-nome="${usuario.nome}">
+                                <button title="Associar protocolos" class="btn btn-sm btn-success assocProt-btn" data-id="${usuario.id}" data-nome="${usuario.nome}">
+                                    <i class="bi bi-file-earmark-medical"></i>
+                                </button>
+                                <button title="Visualizar usuário" class="btn btn-sm btn-primary view-btn" data-id="${usuario.id}" data-nome="${usuario.nome}">
                                     <i class="bi bi-eye"></i>
                                 </button>
-                                <button class="btn btn-sm btn-warning edit-btn" data-id="${usuario.id}" data-nome="${usuario.nome}">
+                                <button title="Editar usuário" class="btn btn-sm btn-warning edit-btn" data-id="${usuario.id}" data-nome="${usuario.nome}">
                                     <i class="bi bi-pencil"></i>
                                 </button>
-                                <button class="btn btn-sm btn-danger delete-btn" data-id="${usuario.id}" data-nome="${usuario.nome}">
+                                <button title="Remover usuário" class="btn btn-sm btn-danger delete-btn" data-id="${usuario.id}" data-nome="${usuario.nome}">
                                     <i class="bi bi-trash"></i>
                                 </button>
                             </td>
@@ -160,4 +165,72 @@ $(document).ready(function(){
         });
     }    
 
+
+    function chamarProtocolos(){
+        $('#usuarioTableBody').on('click', '.assocProt-btn', function() {
+            const idUsuario = $(this).data('id');
+            $('#assocProtIdUsuario').val(idUsuario);
+            
+            // Buscar todas os protocolos disponíveis e preencher o select
+            $.ajax({
+                url: '/api/admin/protocolos',
+                method: 'GET',
+                success: function(protocolos) {
+                    $('#protocolosList').empty();
+                    protocolos.forEach(protocolo => {
+                        $('#protocolosList').append(`
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="${protocolo.idProtocolo}" id="protocolo-${protocolo.idProtocolo}">
+                                <label class="form-check-label" for="protocolo-${protocolo.idProtocolo}">
+                                    ${protocolo.descricao}
+                                </label>
+                            </div>
+                        `);
+                    });
+    
+                    // Buscar e marcar as protocolos já associadas ao usuário
+                    $.ajax({
+                        url: `/api/admin/usuarios/protocolos/${idUsuario}`,
+                        method: 'GET',
+                        success: function(protocolosAssociadas) {
+                            protocolosAssociadas.forEach(protocolo => {
+                                $(`#protocolo-${protocolo.idProtocolo}`).prop('checked', true);
+                            });
+                            $('#assocProtocolosModal').modal('show');
+                        },
+                        error: function(error) {
+                            console.error('Erro ao buscar protocolos do usuário:', error);
+                        }
+                    });
+                },
+                error: function(error) {
+                    console.error('Erro ao buscar protocolos:', error);
+                }
+            });
+        });
+    }
+    function salvarProtocolos(){
+        $('#assocProtocolosForm').submit(function(e) {
+            e.preventDefault();
+            const idUsuario = $('#assocProtIdUsuario').val();
+            const protocolosIds = [];
+            $('#protocolosList input:checked').each(function() {
+                protocolosIds.push($(this).val());
+            });
+    
+            $.ajax({
+                url: `/api/admin/usuarios/protocolos/${idUsuario}`,
+                method: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify({protocolosIds}),
+                success: function() {
+                    $('#assocProtocolosModal').modal('hide');
+                    alert('Associações atualizadas com sucesso');
+                },
+                error: function(error) {
+                    console.error('Erro ao salvar associações:', error);
+                }
+            });
+        });
+    }   
 })
--- model/usuariosAdmin.js
+++ model/usuariosAdmin.js
@@ -102,7 +102,7 @@ module.exports = {
         const updateQueries = categoriasIds.map(idCategoria => `
             INSERT INTO usuario_categoria (usuarioID, categoriaID)
             VALUES (${idUsuario}, ${idCategoria})
-            ON DUPLICATE KEY UPDATE categoriaID = ${idCategoria};
+            ON DUPLICATE KEY UPDATE categoriaID = categoriaID;
         `);
     
         // Atualizar categorias para desassociar do usuário
@@ -121,5 +121,49 @@ module.exports = {
                 console.error('Erro ao associar categorias ao usuário:', err);
                 res.status(500).send('Erro ao associar categorias ao usuário');
             });
-    }
+    },
+    getUsuariosProtocolos: (req, res) => {
+        const id = req.params.id;
+        const query = `
+            SELECT * FROM protocolo
+            JOIN usuario_protocolo ON protocolo.idProtocolo = usuario_protocolo.protocoloID
+            WHERE usuario_protocolo.usuarioID = ${id};
+        `;
+        sequelize.query(query)
+            .then(result => {
+                res.json(result[0]);
+            })
+            .catch(err => {
+                console.error('Erro ao buscar categorias do usuário', err);
+                res.status(500).send('Erro ao buscar categorias do usuário');
+            });
+    },
+    associateUsuariosProtocolos: (req, res) => {
+        const idUsuario = req.params.id;
+        const protocolosIds = req.body.protocolosIds;
+    
+        // Atualizar protocolos para associar ao usuário
+        const updateQueries = protocolosIds.map(idProtocolo => `
+            INSERT INTO usuario_protocolo (usuarioID, protocoloID)
+            VALUES (${idUsuario}, ${idProtocolo})
+            ON DUPLICATE KEY UPDATE protocoloID = protocoloID;
+        `);
+    
+        // Atualizar protocolos para desassociar do usuário
+        const desassociateQuery = `
+            DELETE FROM usuario_protocolo
+            WHERE usuarioID = ${idUsuario}
+            AND protocoloID NOT IN (${protocolosIds.join(', ')});
+        `;
+    
+        Promise.all(updateQueries.map(query => sequelize.query(query)))
+            .then(() => sequelize.query(desassociateQuery))
+            .then(() => {
+                res.status(200).send('Associações atualizadas com sucesso');
+            })
+            .catch(err => {
+                console.error('Erro ao associar protocolos ao usuário:', err);
+                res.status(500).send('Erro ao associar protocolos ao usuário');
+            });
+    },
 }
